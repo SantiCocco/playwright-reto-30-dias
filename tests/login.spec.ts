02f0835e8661374a0b6ef5356c23eaab762e30d0
Santiago Cocco <santiago.cocco95@gmail.com>
2026-06-10 18:17:00 +0200

    refactor reto 1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,24 +1,24 @@
 import { expect, test } from '@playwright/test'
 
-test.describe('Login OrangeHRM', () => {
+test.describe('Login OrangeHRM @login', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/')
   })
 
-  test('Login to hrm @login', async ({ page }) => {
+  test('Login to hrm', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
     await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
     await page.getByRole('button', { name: 'Login' }).click()
     await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
   })
 
-  test('Empty fields @login', async ({ page }) => {
+  test('Empty fields', async ({ page }) => {
     await page.getByRole('button', { name: 'Login' }).click()
     await expect(page.getByText('Required').first()).toBeVisible()
     await expect(page.getByText('Required').nth(1)).toBeVisible()
   })
 
-  test('Invalid credentials @login', async ({ page }) => {
+  test('Invalid credentials', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
     await page.getByRole('textbox', { name: 'Password' }).fill('wrongpassword')
     await page.getByRole('button', { name: 'Login' }).click()
