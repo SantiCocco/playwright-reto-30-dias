1661c844deb03103dfc8bd7df43d91b1cb5f8f32
Santiago Cocco <santiago.cocco95@gmail.com>
2026-06-10 18:15:08 +0200

    refactor reto 1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,31 +1,28 @@
 import { expect, test } from '@playwright/test'
 
-test('Login to hrm @login', async ({ page }) => {
-
+test.describe('Login OrangeHRM', () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/')
+  })
+
+  test('Login to hrm @login', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
     await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
     await page.getByRole('button', { name: 'Login' }).click()
-
     await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
-})
+  })
 
-test('Empty fields @login', async ({ page }) => {
-
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
+  test('Empty fields @login', async ({ page }) => {
     await page.getByRole('button', { name: 'Login' }).click()
-    
     await expect(page.getByText('Required').first()).toBeVisible()
     await expect(page.getByText('Required').nth(1)).toBeVisible()
-})
+  })
 
-test('Invalid credentials @login', async ({ page }) => {
-  
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
+  test('Invalid credentials @login', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
     await page.getByRole('textbox', { name: 'Password' }).fill('wrongpassword')
     await page.getByRole('button', { name: 'Login' }).click()
-    
     await expect(page.getByRole('alert')).toBeVisible()
     await expect(page.getByRole('alert')).toHaveText('Invalid credentials')
+  })
 })
