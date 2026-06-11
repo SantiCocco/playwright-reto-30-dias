2f56e3d158e8adc004765d0e293dfe27ee8bebe8
Santiago Cocco <santiago.cocco95@gmail.com>
2026-06-11 18:00:35 +0200

    reto #3 completado

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "@playwright/test"
+
+test.describe('Validate sidebar items @Navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+  })
+
+  test('Check sidebar options', async ({ page }) => {
+    const sidebarItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentSideBarItemsCount = await sidebarItems.count()
+    console.log('Current sidebar items count', currentSideBarItemsCount)
+
+    const currentSidebarItems: string[] = []
+
+    for (let i = 0; i < currentSideBarItemsCount; i++) {
+      const sidebarText = await sidebarItems.nth(i).innerText()
+      currentSidebarItems.push(sidebarText)
+    }
+    console.log(currentSidebarItems)
+
+    const expectedSidebarItems = [
+      'Admin',
+      'PIM',
+      'Leave',
+      'Time',
+      'Recruitment',
+      'My Info',
+      'Performance',
+      'Dashboard',
+      'Directory',
+      'Maintenance',
+      'Claim',
+      'Buzz']
+    expect(currentSidebarItems).toEqual(expectedSidebarItems)
+    expect(currentSidebarItems[0].toLowerCase()).toBe('admin')
+  })
+})
